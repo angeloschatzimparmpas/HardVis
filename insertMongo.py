8bd3c9131e48d564e8eb3607fa82ade61d0afd8a
Angelos Chatzimparmpas <angelos.chatzimparmpas@gmail.com>
2023-02-08 22:30:56 +0100

    final paper version

--- insertMongo.py
+++ insertMongo.py
@@ -9,17 +9,16 @@ import os
 def import_content(filepath):
     mng_client = pymongo.MongoClient('localhost', 27017)
     mng_db = mng_client['mydb']
-    #collection_name = 'StanceCTest' 
-    collection_name = 'diabetesorig' 
+    collection_name = 'VehicleC' 
     db_cm = mng_db[collection_name]
     cdir = os.path.dirname(__file__)
     file_res = os.path.join(cdir, filepath)
 
     data = pd.read_csv(file_res)
     data_json = json.loads(data.to_json(orient='records'))
-    db_cm.remove()
-    db_cm.insert(data_json)
+    db_cm.drop()
+    db_cm.insert_many(data_json)
 
 if __name__ == "__main__":
-  filepath = '/Users/anchaa/Documents/Research/HardVis_code/extra_data_sets/diabetesorig.csv'
+  filepath = './data/vehicle.csv'
   import_content(filepath)
